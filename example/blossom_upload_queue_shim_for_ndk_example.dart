dbc4be2f878527691cc10995ae8cbbb031141346
Nogringo <npub1kg4sdvz3l4fr99n2jdz2vdxe2mpacva87hkdetv76ywacsfq5leqquw5te@uid.ovh>
2026-05-19 23:46:35 +0200

    first commit

---
+++ example/blossom_upload_queue_shim_for_ndk_example.dart
@@ -0,0 +1,6 @@
+import 'package:blossom_upload_queue_shim_for_ndk/blossom_upload_queue_shim_for_ndk.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/blossom_upload_queue_shim_for_ndk.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/blossom_upload_queue_shim_for_ndk_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
